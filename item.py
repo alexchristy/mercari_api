3fdd95e586f02a853c5970f8ea9da6cb7f76ad9e
Alex <a.christy@ufl.edu>
2022-05-13 17:15:08 -0500

    created item class

---
+++ item.py
@@ -0,0 +1,21 @@
+
+class Item:
+
+    def __init__(self, title, price, shipping, isLocal, isAuth, isSoldOut, condition, brand, catagories, details, desc):
+
+        self.isAuth = isAuth
+        self.isLocal = isLocal
+        self.shipping = shipping
+        self.price = price
+        self.title = title
+        self.condition = condition
+        self.brand = brand
+        self.catagories = catagories
+        self.details = details
+        self.desc = desc
+        self.isSoldOut = isSoldOut
+
+    def __str__(self):
+        return f'(name={self.title}, price={self.price}, shipping={self.shipping}, condition={self.condition}, brand={self.brand})'
+
+    
